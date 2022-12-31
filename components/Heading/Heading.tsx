34e56e2a364020e9f70e37fdf9958e23dbcaffbd
LeSavantR <ruben_hernandez1993@hotmail.es>
2022-12-31 16:42:37 -0500

    Responsive Design Navbar

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -9,16 +9,22 @@ export interface HeadingInterface {
 
 const Heading : React.FC<HeadingInterface> = ({ brand='XKCD', children }) => {
 	return (
-		<Navbar isBordered variant={'floating'}>
+		<Navbar isBordered variant={'sticky'}>
       <Navbar.Brand>
-        <Link href='/'>
+        <Navbar.Toggle showIn={'sm'} aria-label='toggle navigation' />
+        <Link href='/' color={'error'}>
           <AcmeLogo />
-          <Text b color="inherit" hideIn="xs">
+          <Text b hideIn="xs">
               {brand}
           </Text>
         </Link>
       </Navbar.Brand>
-      <Navbar.Content variant={'highlight-rounded'} enableCursorHighlight activeColor={'primary'} hideIn="xs">
+      <Navbar.Content
+        variant={'highlight-rounded'}
+        enableCursorHighlight
+        activeColor={'error'}
+        hideIn="sm"
+      >
         <Navbar.Link href="/">Home</Navbar.Link>
         <Navbar.Link href="/comics/">Comics</Navbar.Link>
         <Navbar.Link href="/search">Search</Navbar.Link>
@@ -27,6 +33,20 @@ const Heading : React.FC<HeadingInterface> = ({ brand='XKCD', children }) => {
       <Navbar.Content>
         {children}
       </Navbar.Content>
+      <Navbar.Collapse transitionTime={20}>
+        <Navbar.CollapseItem>
+          <Link href='/'>Home</Link>
+        </Navbar.CollapseItem>
+        <Navbar.CollapseItem>
+          <Link href='/comics/'>Comics</Link>
+        </Navbar.CollapseItem>
+        <Navbar.CollapseItem>
+          <Link href='/search'>Search</Link>
+        </Navbar.CollapseItem>
+        <Navbar.CollapseItem>
+          <Link href='/about'>About</Link>
+        </Navbar.CollapseItem>
+      </Navbar.Collapse>
     </Navbar>
 	)
 }
